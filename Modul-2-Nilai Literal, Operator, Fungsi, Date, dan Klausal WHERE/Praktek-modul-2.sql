86bdef36d9f84c3cd665714d5b506abc7d4d3e0d
fyy <147676422+AzhariZulfi@users.noreply.github.com>
2025-06-01 08:54:13 +0800

    Update Praktek-modul-2.sql

--- Modul-2-Nilai Literal, Operator, Fungsi, Date, dan Klausal WHERE/Praktek-modul-2.sql
+++ Modul-2-Nilai Literal, Operator, Fungsi, Date, dan Klausal WHERE/Praktek-modul-2.sql
@@ -1,4 +1,4 @@
--- Modul    : 3 Nilai Literal, Operator, Fungsi, Date, dan Klausal WHERE
+-- Modul    : 2 Nilai Literal, Operator, Fungsi, Date, dan Klausal WHERE
 -- Nama     : Muhammad Azhari Zulfiansyah
 -- NIM      : 23241098
 -- Kelas    : PTI C
